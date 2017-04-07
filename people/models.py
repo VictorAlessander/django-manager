642d9016150a729c6121d4ad852be8127cab08fb
Victor Alessander <alessander@protonmail.com>
2017-04-07 11:31:12 -0300

    Little change

--- people/models.py
+++ people/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class MPeople(models.Model):
 
-	name = models.CharField(max_length=100)
+	name = models.CharField(max_length=100, blank=True)
 	address = models.CharField(max_length=255)
 	phone = models.CharField(max_length=13, blank=True)
 	gender = models.CharField(max_length=1)
